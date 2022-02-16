61556e303387b560fbc22b451bc7bb3b9dc59252
Olivia Pomeroy <opomeroy26@gmail.com>
2022-02-16 09:47:28 -0800

    first two deliverables

---
+++ src/index.js
@@ -0,0 +1,50 @@
+//GLOBAL VARIABLE
+const URL = "http://localhost:3000/movies"
+
+//DOM SELECTORS
+const nav = document.querySelector("#movie-list")
+const watchedBtn = document.querySelector("#watched")
+const drops = document.querySelector("#amount")
+
+
+// LISTENERS
+
+//Fetch
+function getAllMovies(){
+    return fetch(URL)
+    .then(resp => resp.json())
+}
+
+//RENDER FUNCTIONS
+function renderMovieArr(movieArr){
+    movieArr.forEach(movieObj=> renderMovieObj(movieObj))
+}
+
+function renderMovieObj(movieObj) {
+    const navImage = document.createElement("img")
+    navImage.src = movieObj.image 
+    nav.appendChild(navImage)
+}
+
+function renderDetail(movieObj){
+    const title = document.querySelector("#title")
+    const year = document.querySelector("#year-released")
+    const description = document.querySelector("#description")
+    const detailImg = document.querySelector("#detail-image")
+    detailImg.src = movieObj.image
+    title.textContent = movieObj.title
+    year.textContent = movieObj.release_year
+    description.textContent = movieObj.description
+    let watchVal;
+    movieObj.watched ? watchValu = "Watched" : watchVal = "Unwatched"
+    watchedBtn.textContent = watchVal
+    drops.textContent = movieObj.blood_amount
+
+}
+
+// HANDLERS
+
+// INITIALIZER
+getAllMovies().then(movieArr => {renderMovieArr(movieArr)
+renderDetail(movieArr[0])
+})
